f0859c9f7d0e69e7516e4183165653384dce35b6
AndersonGomezAza <argomeza@itc.edu.co>
2022-11-21 16:17:14 -0500

    Subiendo Interactividad al front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,53 @@ import { KeycloakService } from 'keycloak-angular';
 export class AppComponent {
   title = 'ProyectoGYM';
 
-  user = '';
-
+  user = '';  role = '';
+  dia: Date; fechaActual:Date; hora = 0; min = 0; seg = 0;
   constructor(private KeycloakService: KeycloakService) { }
 
   ngOnInit(): void {
-    this.ponerNombreUser();
+    this.ponerDatos();
+    this.mostrarFecha();
+    this.irArriba();
   }
 
-  ponerNombreUser(): void {
-    this.user = this.KeycloakService.getKeycloakInstance().realmAccess?.roles[0] + " " + this.KeycloakService.getKeycloakInstance().profile?.firstName +  " " + this.KeycloakService.getKeycloakInstance().profile?.lastName;
+  logout():void{
+    this.KeycloakService.logout();
   }
 
+  ponerDatos(): void {
+    this.role = this.KeycloakService.getKeycloakInstance().realmAccess?.roles[0] + " ";
+    this.user = this.KeycloakService.getKeycloakInstance().profile?.firstName +  " " + this.KeycloakService.getKeycloakInstance().profile?.lastName;
+  }
+
+  mostrarFecha(): void {
+    setInterval(()=>{
+      this.fechaActual = new Date();
+      this.dia = new Date(); 
+      this.seg = this.fechaActual.getSeconds();
+      this.hora = this.fechaActual.getHours();
+      this.min= this.fechaActual.getMinutes();
+    },1000);
+  }
+
+  irArriba():void {
+    let boton = document.querySelector('.ir-arriba') as HTMLElement;
+
+    document.addEventListener("scroll",()=>{
+      if (window.scrollY > 250) {
+        boton.style.display = 'block';
+      } else{
+        boton.style.display = 'none';
+      }
+    });
+
+    boton.addEventListener("click", () => {
+      window.scroll({ 
+        top: 0, 
+        left: 0, 
+        behavior: 'smooth' 
+      });
+    });
+    
+  }
 }
--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -10,11 +10,15 @@ export class SideBarComponent implements OnInit {
 
   role = '';
   user = '';
+
   constructor(private KeycloakService: KeycloakService) { }
 
   ngOnInit(): void {
     this.ponerDatos();
+    this.funcionalidadesRol();
+    this.sideBar(); 
   }
+
   logout():void{
     this.KeycloakService.logout();
   }
@@ -24,4 +28,42 @@ export class SideBarComponent implements OnInit {
     this.user = this.KeycloakService.getKeycloakInstance().profile?.firstName + "";
   }
 
+  funcionalidadesRol():void {
+    var links = document.getElementsByClassName("nav-link") as HTMLCollectionOf<Element>;
+    for (let i = 0; i < links.length; i++) {
+      if (this.role == "Usuario") {
+        if (links[i].classList[1] == "administrador") {
+          links[i].classList.remove("link-block-sidebar");
+        }else if (links[i].classList[1] == "entrenador" ) {
+          links[i].classList.remove("link-block-sidebar");
+        }
+      } else if (this.role == "Entrenador") {
+        if (links[i].classList[1] == "administrador") {
+          links[i].classList.remove("link-block-sidebar");
+        }
+      }
+    }
+  }
+
+  sideBar():void {
+    const body = document.querySelector('body') as HTMLBodyElement,
+    sidebar = body.querySelector('nav') as HTMLElement,
+    toggle = body.querySelector(".toggle") as Element,
+    modeSwitch = body.querySelector(".toggle-switch") as Element,
+    modeText = body.querySelector(".mode-text") as HTMLElement;
+
+    toggle.addEventListener("click" , () =>{
+        sidebar.classList.toggle("close");
+    })
+    
+    modeSwitch.addEventListener("click" , () =>{
+        body.classList.toggle("dark");
+        
+        if(body.classList.contains("dark")){
+          modeText.innerText = 'Light mode';
+        }else{
+          modeText.innerText = 'Dark mode';
+        }
+    });
+  }
 }
